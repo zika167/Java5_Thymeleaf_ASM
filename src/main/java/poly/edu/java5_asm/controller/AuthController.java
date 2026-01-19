ae835d6ef6bad5d9ba2101c4e8a42ea5796f4207
ChenZhouThinh2636 <thinhtran2632006@gmail.com>
2026-01-19 13:05:43 +0700

    DevThinh commit2: Fix Auth

--- src/main/java/poly/edu/java5_asm/controller/AuthController.java
+++ src/main/java/poly/edu/java5_asm/controller/AuthController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import poly.edu.java5_asm.dto.RegisterRequest;
+import poly.edu.java5_asm.dto.request.RegisterRequest;
 import poly.edu.java5_asm.service.AuthService;
 
 /**
